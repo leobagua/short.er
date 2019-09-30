f03c300fd6b453a3c0b9d3f01016f4f424d48ce6
Leonardo Rosa <leonardodarosa@unochapeco.edu.br>
2019-09-30 10:05:40 -0300

    #2: Add APP_BASE_URL and Changed the Url.js path.

--- app/assets/javascripts/app/urls/urls.js
+++ app/assets/javascripts/app/urls/urls.js
@@ -9,6 +9,6 @@ $(function () {
     $('.url-card').first().click();
 
     function getUrl(urlId) {
-        $.get('app/urls/' + urlId);
+        $.get(APP_BASE_URL + 'app/urls/' + urlId);
     }
 });
